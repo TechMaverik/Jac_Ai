fd288b80e33c27c4c7a00b1b8918e05b97ec39f0
TechMaverik <informakhiljacob@gmail.com>
2024-03-18 01:32:34 +0530

    updated model

---
+++ models/expenses.py
@@ -0,0 +1,26 @@
+"""expenses.py"""
+
+from pydantic import BaseModel
+from typing import Optional
+
+
+class ExpensesReq(BaseModel):
+    """Expense request model
+
+    Args:
+        to (str): To account
+        source (str): From account
+        expense_type(str): Expense type
+        expense_amount(float): Expense amount
+        description(str): Expense description
+        is_monthly_expense: Is monthly expense status
+        is_consumer_bill: Is consumer bill status
+    """
+
+    spent_to: str
+    account: str
+    expense_type: str
+    expense_amount: float
+    description: str
+    is_monthly_expense: bool
+    is_consumer_bill: bool
