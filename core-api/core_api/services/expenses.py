cb2fa2236b71712c7d162156388705b88f926bd7
TechMaverik <informakhiljacob@gmail.com>
2024-03-29 10:28:14 +0530

    generate random primary ids

--- core-api/core_api/services/expenses.py
+++ core-api/core_api/services/expenses.py
@@ -1,6 +1,12 @@
+import random
 from core_api.mappers import expenses as expenses_mappers
 from core_api.models.expenses import ExpensesReq
 from core_api.configurations import menu_config
+
+
+def create_id() -> int:
+    generated_id = random.randint(1, 999999999999)
+    return generated_id
 
 
 def is_monthly_expense(expense_type: str) -> bool:
@@ -43,7 +49,7 @@ def put_to_expenses_service(expensesReq: ExpensesReq) -> bool:
         "description": expensesReq.description,
         "is_monthly_expense": is_monthly_expense(expensesReq.expense_type),
         "is_consumer_bill": is_consumer_bill(expensesReq.expense_type),
-        "ID": expensesReq.id,
+        "ID": create_id(),
     }
     return expenses_mappers.put_to_expenses_mapper(item)
 
